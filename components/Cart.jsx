ade2479721db01cfe2cba059f5865924b2ed9abe
Roman <112611445+Makar789654@users.noreply.github.com>
2023-03-25 19:40:29 +0100

    Add files via upload

---
+++ components/Cart.jsx
@@ -0,0 +1,12 @@
+import React, { useRef } from 'react';
+import Link from 'next/link';
+
+
+
+const Cart = () => {
+  return (
+    <div>Cart</div>
+  )
+}
+
+export default Cart
---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+// import { AiOutlineShopping } from 'react-icons/ai'
+
+// import { Cart } from './';
+// import { useStateContext} from '../context/StateContext';
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
