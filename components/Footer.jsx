01476a3781da240841ea7c387bddba929b756f13
Makar789654 <112611445+Makar789654@users.noreply.github.com>
2023-03-27 17:31:39 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { AiFillInstagram, AiOutlineTwitter, AiFillYoutube ,AiOutlineGithub} from 'react-icons/ai';
 
 const Footer = () => {
@@ -6,10 +7,18 @@ const Footer = () => {
     <div className="footer-container">
       <p>2023 Makar All rights not reserverd</p>
       <p className="icons">
+        <Link href='https://www.instagram.com/'>
         <AiFillInstagram />
+        </Link>
+        <Link href='https://twitter.com/'>
         <AiOutlineTwitter />
+        </Link>
+        <Link href='https://www.youtube.com/'>
         <AiFillYoutube />
+        </Link>
+        <Link href='https://github.com/'>
         <AiOutlineGithub />
+        </Link>
       </p>
     </div>
   )
